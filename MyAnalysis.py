3d8c75723b464c7d676899212965396954eec971
Piero Viscone <pieroviscone@gmail.com>
2026-05-05 00:02:42 +0200

    implemented new observables

--- MyAnalysis.py
+++ MyAnalysis.py
@@ -68,6 +68,12 @@ class MyAnalysis(object):
         ).Filter(f"NJet >= {njetCut}")
         # reorder jets by pt
         self.rdf = SortCollection(self.rdf, "Jet", sort_by="Jet_pt")
+        # select up to 4 jets (with highest pt)
+        self.rdf = FilterCollection(
+            self.rdf,
+            "Jet",
+            indices="ROOT::VecOps::Range(NJet > 4 ? 4 : NJet)",
+        )
 
         #! ================ Muon selection ================
         ###  - Select events with at least 1 isolated muon
@@ -95,6 +101,116 @@ class MyAnalysis(object):
         )
 
         #! ============= Observables definition ============
+        self.rdf = self.rdf.Define(
+            "MedianJet_dR",
+            """
+            ROOT::RVecF dRs;
+            auto comb = ROOT::VecOps::Combinations(Jet_eta, 2);
+            for (size_t &i1 : comb[0]) {
+                for (size_t &i2 : comb[1]) {
+                    float dR = ROOT::VecOps::DeltaR(Jet_eta[i1], Jet_eta[i2], Jet_phi[i1], Jet_phi[i2]);
+                    dRs.push_back(dR);
+                }
+            }
+            // Return median dR or -1 if no combinations
+            if (dRs.size() == 0) {
+                return (float) -1.0;
+            }
+            std::sort(dRs.begin(), dRs.end());
+            if (dRs.size() % 2 == 1) {
+                return (float) dRs[dRs.size() / 2];
+            } else {
+                return (float) 0.5 * (dRs[dRs.size() / 2 - 1] + dRs[dRs.size() / 2]);
+            }
+            """,
+        )
+
+        self.rdf = self.rdf.Define(
+            "InvariantMass_LeastBtaggedJets",
+            """
+            if (NJet < 2) {return (float) -1.0;}
+            ROOT::RVecI sorted_indices = ROOT::VecOps::Argsort(Jet_btag);
+            return ROOT::VecOps::InvariantMasses(
+                ROOT::RVecF{Jet_pt[sorted_indices[0]]},
+                ROOT::RVecF{Jet_eta[sorted_indices[0]]},
+                ROOT::RVecF{Jet_phi[sorted_indices[0]]},
+                ROOT::RVecF{Jet_mass[sorted_indices[0]]},
+                ROOT::RVecF{Jet_pt[sorted_indices[1]]},
+                ROOT::RVecF{Jet_eta[sorted_indices[1]]},
+                ROOT::RVecF{Jet_phi[sorted_indices[1]]},
+                ROOT::RVecF{Jet_mass[sorted_indices[1]]})[0]
+            """,
+        )
+
+        self.rdf = self.rdf.Define(
+            "MinInvariantMass_Jets",
+            """
+            if (NJet < 2) {return (float) -1.0;}
+            auto comb = ROOT::VecOps::Combinations(Jet_eta, 2);
+            ROOT::RVecF invMasses;
+            for (size_t &i1 : comb[0]) {
+                for (size_t &i2 : comb[1]) {
+                    float invM = ROOT::VecOps::InvariantMasses(
+                        ROOT::RVecF{Jet_pt[i1]},
+                        ROOT::RVecF{Jet_eta[i1]},
+                        ROOT::RVecF{Jet_phi[i1]},
+                        ROOT::RVecF{Jet_mass[i1]},
+                        ROOT::RVecF{Jet_pt[i2]},
+                        ROOT::RVecF{Jet_eta[i2]},
+                        ROOT::RVecF{Jet_phi[i2]},
+                        ROOT::RVecF{Jet_mass[i2]})[0];
+                    invMasses.push_back(invM);
+                }
+            }
+            if (invMasses.size() == 0) {
+                return (float) -1.0;
+            }
+            return ROOT::VecOps::Min(invMasses);
+            """,
+        )
+
+        self.rdf = self.rdf.Define(
+            "InvariantMass_LeadingMuon_MostbTaggedJet",
+            """
+            if (NJet < 1) {return (float) -1.0;}
+            ROOT::RVecI sorted_indices = ROOT::VecOps::Reverse(ROOT::VecOps::Argsort(Jet_btag));
+            return ROOT::VecOps::InvariantMasses(
+                ROOT::RVecF{LeadingMuon_pt},
+                ROOT::RVecF{LeadingMuon_eta},
+                ROOT::RVecF{LeadingMuon_phi},
+                ROOT::RVecF{0.105}, // muon mass
+                ROOT::RVecF{Jet_pt[sorted_indices[0]]},
+                ROOT::RVecF{Jet_eta[sorted_indices[0]]},
+                ROOT::RVecF{Jet_phi[sorted_indices[0]]},
+                ROOT::RVecF{Jet_mass[sorted_indices[0]]})[0]
+            """,
+        )
+
+        self.rdf = self.rdf.Define(
+            "dR_LeadingMuon_MostbTaggedJet",
+            """
+            if (NJet < 1) {return (float) -1.0;}
+            ROOT::RVecI sorted_indices = ROOT::VecOps::Reverse(ROOT::VecOps::Argsort(Jet_btag));
+            return ROOT::VecOps::DeltaR(
+                LeadingMuon_eta, Jet_eta[sorted_indices[0]],
+                LeadingMuon_phi, Jet_phi[sorted_indices[0]]
+            );
+            """,
+        )
+
+        self.rdf = self.rdf.Define(
+            "dR_LeastBtaggedJets",
+            """
+            if (NJet < 2) {return (float) -1.0;}
+            ROOT::RVecI sorted_indices = ROOT::VecOps::Argsort(Jet_btag);
+            return ROOT::VecOps::DeltaR(
+                Jet_eta[sorted_indices[0]], Jet_eta[sorted_indices[1]],
+                Jet_phi[sorted_indices[0]], Jet_phi[sorted_indices[1]]
+            );
+            """,
+        )
+
+        self.rdf = self.rdf.Define("HT", "ROOT::VecOps::Sum(Jet_pt)")
 
     def processEvents(self):
         # Implement additional cuts
@@ -120,6 +236,35 @@ class MyAnalysis(object):
         )
         self.defineHisto("NMuon", "# of isolated muons", (5, 0.5, 5.5))
         self.defineHisto("BDTscore", "BDT score", (25, -1.0, 1.0))
+        self.defineHisto("MedianJet_dR", "Median dR between jets", (25, 0.0, 5.0))
+        self.defineHisto(
+            "InvariantMass_LeastBtaggedJets",
+            "Invariant mass of least b-tagged jets [GeV]",
+            (25, 0.0, 200.0),
+        )
+        self.defineHisto(
+            "InvariantMass_LeadingMuon_MostbTaggedJet",
+            "Invariant mass of leading muon and most b-tagged jet [GeV]",
+            (25, 0.0, 200.0),
+        )
+
+        self.defineHisto(
+            "dR_LeadingMuon_MostbTaggedJet",
+            "dR between leading muon and most b-tagged jet",
+            (25, 0.0, 5.0),
+        )
+
+        self.defineHisto(
+            "dR_LeastBtaggedJets",
+            "dR between least b-tagged jets",
+            (25, 0.0, 5.0),
+        )
+        self.defineHisto(
+            "MinInvariantMass_Jets",
+            "Minimum invariant mass of jet pairs [GeV]",
+            (25, 0.0, 200.0),
+        )
+        self.defineHisto("HT", "HT [GeV]", (25, 0.0, 500.0))
 
     def saveHistos(self):
         # Trigger computation of all histograms in parallel using RunGraphs
