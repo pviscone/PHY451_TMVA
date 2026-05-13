fd1e9e52bf7888e40570a4b826d546754be0076c
Piero Viscone <pvisco@farm-ui1.physik.uzh.ch>
2026-05-13 14:49:41 +0200

    Switched to conda setup on uzh cluster

--- setup_uzh.sh
+++ setup_uzh.sh
@@ -1,27 +1,45 @@
 DIR="$( dirname -- "${BASH_SOURCE[0]}"; )";
 DIR="$( realpath -e -- "$DIR"; )";
 
-source /app/cern/root_v6.32.10/bin/thisroot.sh
-alias python="python3.11"
-export PATH=$DIR/Combine/build/bin:$PATH
-export LD_LIBRARY_PATH=$DIR/Combine/build/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$DIR/Combine/build/python:$PYTHONPATH
+#source /app/cern/root_v6.32.10/bin/thisroot.sh
 
+#add micromamba (from pviscone local) and combine binary and lib (fallback on pviscone)
+export PATH=/home/uzh/pvisco/.local/bin:$HOME/Combine/build/bin:/home/uzh/pvisco/Combine/build/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/Combine/build/lib:/home/uzh/pvisco/Combine/build/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$HOME/Combine/build/python:/home/uzh/pvisco/Combine/build/python:$PYTHONPATH
+
+alias python="python3.12"
+
+#micromamba create -n combine python=3.12 root=6.34 gsl boost-cpp vdt eigen tbb cmake ninja pandas
+eval "$(micromamba shell hook --shell bash)"
+micromamba activate /home/uzh/pvisco/.local/share/mamba/envs/combine
 
 if [[ " $@ " =~ " clean " ]]; then
-    rm -rf $DIR/Combine
+    rm -rf $HOME/Combine
 fi
 
-if [[ " $@ " =~ " build " ]]; then
-    git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git Combine
-    cd Combine
+if [[ " $@ " =~ " combine " ]]; then
+    git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git $HOME/Combine
+    cd $HOME/Combine
     git checkout v10.6.0
-    sed -i "s|find_package(Boost CONFIG REQUIRED COMPONENTS program_options filesystem)|find_package(Boost REQUIRED COMPONENTS program_options filesystem)|g" CMakeLists.txt
+
     mkdir build
     cd build
-    cmake .. -DUSE_VDT=FALSE
-    cmake --build . -j8
-    cd ../..
-    sed -i "s|/usr/bin/env python3|/usr/bin/python3.11|g" $DIR/Combine/build/bin/*.py
-    python3.11 -m pip install pandas
+    cmake .. -DUSE_VDT=FALSE -DBOOST_ROOT=/home/uzh/pvisco/boost-1.86.0/build \
+          -DBoost_DIR=/home/uzh/pvisco/boost-1.86.0/build/lib/cmake/Boost-1.86.0 \
+          -DCMAKE_PREFIX_PATH=/home/uzh/pvisco/boost-1.86.0/build
+    cmake --build . -j8 
+    cd $DIR
+fi
+
+if [[ " $@ " =~ " combine_conda " ]]; then
+    git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git $HOME/Combine
+    cd $HOME/Combine
+    git checkout v10.6.0
+
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCMAKE_INSTALL_PYTHONDIR=lib/python3.12/site-packages -DUSE_VDT=OFF
+    cmake --build . -j8 
+    cd $DIR
 fi
